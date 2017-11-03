e4cd7aeff1e5176144bfe19392087a0a01564c13
kungkk <kungkk77@gmail.com>
2017-11-03 10:25:22 +0800

    finalize the code

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,34 @@
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/', function () {
+    $query = http_build_query([
+        'client_id' => 4,        
+        'redirect_uri' => 'http://localhost:9000/callback',        
+        'response_type' => 'code',        
+        'scope' => '',    
+    ]);
+    return redirect('http://localhost:8000/oauth/authorize?'.$query);
+});
+
+
+Route::get('/callback', function (\Illuminate\Http\Request $request) {
+    $http = new GuzzleHttp\Client;
+    
+    $response = $http->post('http://localhost:8000/oauth/token', [
+        'form_params' => [
+            'grant_type' => 'authorization_code',            
+            'client_id' => 4,            
+            'client_secret' => 'r946RbzBVY0WUGbofng2E7thLdkU1GQGHYtYHrLO',            
+            'redirect_uri' => 'http://localhost:9000/callback',            
+            'code' => $request->code]    
+    ]);
+    return json_decode((string) $response->getBody(),true);    
+
 });
